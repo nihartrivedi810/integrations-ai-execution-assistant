dbcefd086bb2a1bad70f2c5c8e90c5a14d2c6dff
Nihar Trivedi <nihar.trivedi888@gmail.com>
2026-02-15 12:56:18 -0600

    cleanup - fresh start

--- backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getHealth } from "../controllers/healthController";
-
-const router = Router();
-
-router.get("/health", getHealth);
-
-export { router as rootRouter };
